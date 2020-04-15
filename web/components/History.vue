0464caaa8817715e73719fc28f9dec42b38e7ab2
Luiza Bomfim <bomfimluiza@gmail.com>
2020-04-15 18:12:17 -0300

    Refactor index page

---
+++ web/components/History.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-card
+    class="mt-3 history animated slideInUp"
+    header="Payment History"
+    header-bg-variant="primary"
+    border-variant="primary"
+    header-text-variant="white"
+  >
+    <div class="scroll-y">
+      <b-table striped hover :items="items"></b-table>
+    </div>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "history",
+  props: {
+    items: Array
+  }
+}
+</script>
+
+<style>
+.history {
+  max-height: 60vh;
+}
+
+.history .card-body {
+  overflow-y: scroll;
+}
+</style>
---
+++ web/components/NewPaymentButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-button size="sm" variant="success" @click="onClick">
+    New payment
+  </b-button>
+</template>
+
+<script>
+export default {
+  name: "newPaymentButton",
+  methods: {
+    onClick() {
+      this.$emit('click');
+    }
+  }
+}
+</script>
